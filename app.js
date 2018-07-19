72e3feb3cd4b37cf684e55aa20f201f2c9c73af0
tsubasahonda <lenonystalicelight+tsubasahonda@gmail.com>
2018-07-19 16:44:13 +0900

    jsonからsqlファイル作れるようにした

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 const mysql = require('mysql');
+const fs = require('fs');
+let rstoredata = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+
+console.log(rstoredata['225']['所在地']);
 
 const con = mysql.createConnection({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
   password: 'password',
-  database: 'node_test'
+  database: 'rstore'
 });
 
-// 接続
+let rstoredataId = new Array();
+for(key in rstoredata) {
+  rstoredataId.push(key);
+}
+
 con.connect((err) => {
   if (err) throw err;
 
   console.log('connected to mysql');
 });
 
+for(i=0; i < rstoredataId.length; i++) {
+  let id = rstoredataId[i];
 
-let data = {
-  value: Math.random()
-};
+  let data = {
+    'ID': rstoredata[id]['物件No.'],
+    'Name': rstoredata[id]['物件No.'],
+    'Rent': Number(rstoredata[id]['家賃'].replace(/[^0-9]/g, '')),
+    'Space': rstoredata[id]['面積'].replace(/[^\d+\.\d+]/, ''),
+    'Address': rstoredata[id]['所在地']
+  }
 
-// insert
-con.query('insert into scores set ?', data, (err, res)  => {
-  if (err) throw err;
-  console.log(data.value);
-});
+  con.query('insert into rstore set ?', data, (err, res)  => {
+    if (err) throw err + data.ID;
+    console.log('success!   ' + data.ID);
+  });
+}
 
-// 切断
 con.end((err) => {
   if (err) throw err;
 
